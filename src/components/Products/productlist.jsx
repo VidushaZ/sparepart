ed22dce1a357c39789a63eb3080929b2d12bfab0
Vidusha <Vidushazoro@gmail.com>
2021-09-12 11:51:37 +0530

    Front page Alignment added

--- src/components/Products/productlist.jsx
+++ src/components/Products/productlist.jsx
@@ -10,7 +10,7 @@ import Collapse from '@material-ui/core/Collapse';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import { red , green} from '@material-ui/core/colors';
+import { red, green } from '@material-ui/core/colors';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
@@ -18,91 +18,90 @@ import MoreVertIcon from '@material-ui/icons/MoreVert';
 import Tooltip from '@material-ui/core/Tooltip';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    maxWidth: 345,
-  },
-  media: {
-    height: 0,
-    paddingTop: '56.25%', // 16:9
-    
-  },
-  expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)',
-  },
-  avatarRed: {
-    backgroundColor: red[500],
-  },
-  avatarGreen: {
-    backgroundColor: green[500],
-  },
+	root: {
+		maxWidth: 345
+	},
+	media: {
+		height: 0,
+		paddingTop: '56.25%' // 16:9
+	},
+	expand: {
+		transform: 'rotate(0deg)',
+		marginLeft: 'auto',
+		transition: theme.transitions.create('transform', {
+			duration: theme.transitions.duration.shortest
+		})
+	},
+	expandOpen: {
+		transform: 'rotate(180deg)'
+	},
+	avatarRed: {
+		backgroundColor: red[500]
+	},
+	avatarGreen: {
+		backgroundColor: green[500]
+	}
 }));
 
 export default function RecipeReviewCard(props) {
-  const classes = useStyles();
-  const [expanded, setExpanded] = React.useState(false);
+	const classes = useStyles();
+	const [ expanded, setExpanded ] = React.useState(false);
 
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
-  
-  return (
-    <Card className={classes.root}>
-      <CardHeader
-      
-        avatar={
-          {...props.product.isAvailable ?  <Tooltip title="Available" placement="top"><Avatar aria-label="recipe" className={classes.avatarGreen}>
-          A
-        </Avatar></Tooltip> : <Tooltip title="Out of Stock" ><Avatar aria-label="recipe" className={classes.avatarRed}>
-        O
-      </Avatar>
-      </Tooltip>
-      }
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title={props.product.name}
-        subheader={props.product.category}
-      />
-      <CardMedia
-        className={classes.media}
-        image={props.product.imgUrl}
-      />
-      
-      <CardActions disableSpacing>
-        Rs {props.product.price}
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton
-          className={clsx(classes.expand, {
-            [classes.expandOpen]: expanded,
-          })}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </IconButton>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>Info:</Typography>
-          
-          <Typography paragraph>
-            {props.product.description}
-          </Typography>
-        </CardContent>
-      </Collapse>
-    </Card>
-  );
+	const handleExpandClick = () => {
+		setExpanded(!expanded);
+	};
+
+	return (
+		<Card className={classes.root}>
+			<CardHeader
+				avatar={{
+					...(props.product.isAvailable ? (
+						<Tooltip title="Available" placement="top">
+							<Avatar aria-label="recipe" className={classes.avatarGreen}>
+								A
+							</Avatar>
+						</Tooltip>
+					) : (
+						<Tooltip title="Out of Stock">
+							<Avatar aria-label="recipe" className={classes.avatarRed}>
+								O
+							</Avatar>
+						</Tooltip>
+					))
+				}}
+				action={
+					<IconButton aria-label="settings">
+						<MoreVertIcon />
+					</IconButton>
+				}
+				title={props.product.name}
+				subheader={props.product.category}
+			/>
+			<CardMedia className={classes.media} image={props.product.imgUrl} />
+
+			<CardActions disableSpacing>
+				Rs {props.product.price}
+				<IconButton aria-label="add to favorites">
+					<FavoriteIcon />
+				</IconButton>
+				<IconButton
+					className={clsx(classes.expand, {
+						[classes.expandOpen]: expanded
+					})}
+					onClick={handleExpandClick}
+					aria-expanded={expanded}
+					aria-label="show more"
+				>
+					<ExpandMoreIcon />
+				</IconButton>
+			</CardActions>
+			<Collapse in={expanded} timeout="auto" unmountOnExit>
+				<CardContent>
+					<Typography paragraph>Info:</Typography>
+
+					<Typography paragraph>{props.product.description}</Typography>
+				</CardContent>
+			</Collapse>
+		</Card>
+	);
 }
